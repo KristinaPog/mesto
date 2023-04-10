30a3133a2c3f6089d9acdd852604ba214abda91d
Kristina Pogodina <pogodina.kristi@yandex.ru>
2023-04-10 10:10:49 +0300

    Произведён рефакторинг кода

--- scripts/Card.js
+++ scripts/Card.js
@@ -1,48 +1,49 @@
-export class Card { 
-  constructor (cardData, templateSelector, handleImageClick){ 
-    this._name = cardData.name; 
-    this._link = cardData.link; 
-    this._templateSelector = templateSelector; 
+export class Card {
+  constructor(cardData, templateSelector, handleImageClick) {
+    this._name = cardData.name;
+    this._link = cardData.link;
+    this._templateSelector = templateSelector;
     this._handleImageClick = handleImageClick;
-  }  
+  }
 
-  _getTemplate(){ 
-    const newPlace = document.querySelector(this._templateSelector).content.querySelector('.place').cloneNode(true); 
+  _getTemplate() {
+    const newPlace = document.querySelector(this._templateSelector).content.querySelector('.place').cloneNode(true);
     return newPlace;
-  } 
+  }
 
-  _setLikeEventListener () { 
-    const buttonLike = this._element.querySelector('.like'); 
-    buttonLike.addEventListener('click', function(evt){evt.target.classList.toggle('like_active')}); 
-  } 
+  _setLikeEventListener() {
+    const buttonLike = this._element.querySelector('.like');
+    buttonLike.addEventListener('click', function (evt) { evt.target.classList.toggle('like_active') });
+  }
 
-  _setDeleteEventListener() { 
-    const trash = this._element.querySelector ('.trash'); 
-    trash.addEventListener('click', ()=>{
-      this._element.remove(); 
-      this._element = null;}); 
-  } 
+  _setDeleteEventListener() {
+    const trash = this._element.querySelector('.trash');
+    trash.addEventListener('click', () => {
+      this._element.remove();
+      this._element = null;
+    });
+  }
 
   _openImage() {
     const image = this._element.querySelector('.place__image');
-    image.addEventListener('click', ()=>{
+    image.addEventListener('click', () => {
       this._handleImageClick(this._name, this._link);
     });
   }
 
-  _setEventListeners () {
-    this._setLikeEventListener(); 
-    this._setDeleteEventListener(); 
+  _setEventListeners() {
+    this._setLikeEventListener();
+    this._setDeleteEventListener();
     this._openImage();
   }
 
-  generateCard(){ 
-    this._element = this._getTemplate(); 
+  generateCard() {
+    this._element = this._getTemplate();
     const image = this._element.querySelector('.place__image');
     const label = this._element.querySelector('.place__text');
-    image.src = this._link; 
-    label.textContent = this._name; 
-    image.alt = this._name; 
+    image.src = this._link;
+    label.textContent = this._name;
+    image.alt = this._name;
     this._setEventListeners();
     return this._element;
   }
